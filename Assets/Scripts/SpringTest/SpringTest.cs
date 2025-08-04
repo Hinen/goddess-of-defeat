3eb1db42499e9fc6107fd80f7aee2b2a12f5bcb5
Hinen <qkrqldtjqj@gmail.com>
2025-08-04 20:49:59 +0900

    탄성도 및 spring force 추가

--- Assets/Scripts/SpringTest/SpringTest.cs
+++ Assets/Scripts/SpringTest/SpringTest.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class SpringTest : MonoBehaviour {
     [SerializeField]
+    private GameObject dot;
+    
+    [SerializeField]
     private float gravity = -9.81f;
 
     [SerializeField]
     private float mass = 1.0f;
-    
+
+    [SerializeField]
+    private float stiffness = 7f;
+   
     private float _velocityY;
-    
+
     private void FixedUpdate() {
-        var forceY = gravity * mass;
+        var displacement = transform.position.y - dot.transform.position.y;
+        var springForceY = -stiffness * displacement;
+        var forceY = springForceY + gravity * mass;
+        
         _velocityY += forceY * Time.fixedDeltaTime;
         gameObject.transform.position += new Vector3(0, _velocityY * Time.fixedDeltaTime, 0);
     }
