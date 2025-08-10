204cd2e2946415e30f79dc4db2ee57c6ef09caa0
Hinen <qkrqldtjqj@gmail.com>
2025-08-10 17:19:53 +0900

    NaN 방지

--- Assets/Scripts/Game/Bones/SpringBone.cs
+++ Assets/Scripts/Game/Bones/SpringBone.cs
@@ -12,8 +12,6 @@ namespace Game.Bones {
         private float damping = 1f;
         
         private Vector3 _setupPosition;
-        private Vector3 _oldPosition;
-        
         private Vector3 _velocity;
 
         private void Start() {
@@ -21,20 +19,20 @@ namespace Game.Bones {
         }
         
         protected override void ApplySpringForcePosition(Vector3 positionDiff) {
-            _oldPosition = transform.localPosition;
+            var oldPosition = transform.localPosition;
             
-            var position= _oldPosition + positionDiff;
+            var position = oldPosition + positionDiff;
             var displacement = position - _setupPosition;
             var springForce = -stiffness * displacement;
             var dampingForce = damping * _velocity;
             var force = springForce - dampingForce;
-            var acceleration = force / mass;
-            
+            var acceleration = mass != 0f ? force / mass : force;
+
             _velocity += acceleration * Time.fixedDeltaTime;
             position += _velocity * Time.fixedDeltaTime;
             transform.localPosition = position;
             
-            var diff = position - _oldPosition;
+            var diff = position - oldPosition;
             base.ApplySpringForcePosition(diff);
         }
     }
