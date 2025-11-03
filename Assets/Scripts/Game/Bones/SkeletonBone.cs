ff03376c8a77f19c502d733546a34dab7af1f430
Hinen <qkrqldtjqj@gmail.com>
2025-11-03 21:41:46 +0900

    헤어 계단 현상을 줄이기 위해서 변경되는 자식 본 위치에 맞게 부모 본 회전 처리

--- Assets/Scripts/Game/Bones/SkeletonBone.cs
+++ Assets/Scripts/Game/Bones/SkeletonBone.cs
@@ -1,8 +1,14 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Bones {
     public class SkeletonBone : MonoBehaviour {
+        private struct SetupData {
+            public Vector3 SkeletonPosition;
+            public Quaternion Rotation;
+        }
+        
         public enum PositionMixType {
             Additive,
             Mean,
@@ -12,29 +18,66 @@ namespace Game.Bones {
         
         [SerializeField]
         private PositionMixType positionMixType;
+        
+        [SerializeField]
+        private bool isApplyRotationToParentBone = true;
 
+        private SetupData _parentSetupData;
+        private SetupData _setupData;
+
+        private Skeleton.Skeleton _skeleton;
         private AnimationBoneManager _animationBoneManager;
         private SpringBone _springBone;
         private AnimationBone AnimationBone => _animationBoneManager.Mapping.GetValueOrDefault(gameObject);
+        private Vector3 ToSkeletonPosition(Vector3 worldPosition) => _skeleton.transform.InverseTransformPoint(worldPosition);
         
         public Vector3 Position {
             set => transform.position = value;
         }
 
         private void Awake() {
-            _animationBoneManager = GetComponentInParent<AnimationBoneManager>();
+            _skeleton = GetComponentInParent<Skeleton.Skeleton>();
+            _animationBoneManager = _skeleton.GetComponent<AnimationBoneManager>();
             _springBone = GetComponent<SpringBone>();
+
+            _parentSetupData = new SetupData {
+                SkeletonPosition = ToSkeletonPosition(transform.parent.position),
+                Rotation = transform.parent.rotation
+            };
+            
+            _setupData = new SetupData {
+                SkeletonPosition = ToSkeletonPosition(transform.position),
+                Rotation = transform.rotation
+            };
         }
 
         private void LateUpdate() {
-            if (positionMixType == PositionMixType.Additive)
-                Position = AnimationBone.Position + _springBone.SetupOffset;
-            else if (positionMixType == PositionMixType.Mean)
-                Position = Vector3.Lerp(_springBone.Position, AnimationBone.Position, 0.5f);
-            else if (positionMixType == PositionMixType.Override)
-                Position = _springBone.Position;
-            else if (positionMixType == PositionMixType.None)
-                Position = AnimationBone.Position;
+            var position = positionMixType switch {
+                PositionMixType.Additive => AnimationBone.Position + _springBone.SetupOffset,
+                PositionMixType.Mean => Vector3.Lerp(_springBone.Position, AnimationBone.Position, 0.5f),
+                PositionMixType.Override => _springBone.Position,
+                PositionMixType.None => AnimationBone.Position,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            if (isApplyRotationToParentBone && positionMixType != PositionMixType.None)
+                ParentBoneRotation(ToSkeletonPosition(position));
+            
+            Position = position;
+        }
+        
+        private void ParentBoneRotation(Vector3 skeletonPosition) {
+            var setupDirection = _setupData.SkeletonPosition - _parentSetupData.SkeletonPosition;
+            var setupRotation = Quaternion.Inverse(_parentSetupData.Rotation) * setupDirection;
+            var setupAngle = Mathf.Atan2(setupRotation.y, setupRotation.x) * Mathf.Rad2Deg;
+            
+            // current
+            var direction = skeletonPosition - _parentSetupData.SkeletonPosition;
+            var rotation = Quaternion.Inverse(_parentSetupData.Rotation) * direction;
+            var angle = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+            var delta = Mathf.DeltaAngle(setupAngle, angle);
+            transform.parent.rotation = _parentSetupData.Rotation * Quaternion.Euler(0f, 0f, delta);
         }
     }
 }
